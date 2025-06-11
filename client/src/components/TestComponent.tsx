47e4b8cf4413fba65281e83de9e58566cad7330c
aquaman122 <swj981128@gmail.com>
2025-06-11 14:59:38 +0900

    feat: ErrorMessage, input 컴포넌트 생성

--- client/src/components/TestComponent.tsx
+++ client/src/components/TestComponent.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import { useQuery } from "@tanstack/react-query";
 
 const TestComponent = () => {
@@ -23,6 +24,42 @@ const TestComponent = () => {
           <div>{post.content}</div>
         </div>
       ))}
+      <div className="w-[36.875rem] rounded-lg shadow-lg p-8 border-2">
+        <div className="space-y-4">
+
+          <img 
+            src="/image/logo.png"
+            alt="로고"
+            className="w-[15rem] h-[8rem]"
+          />
+
+          <div className="flex">
+            <h1 className="text-2xl font-semibold">로그인</h1>
+          </div>
+
+          <form className="flex flex-col gap-5">            
+            <Input
+              id="비밀번호"
+              type="text"
+              placeholder="아이디"
+              error=""
+            />
+            <Input
+              id="비밀번호"
+              type="text"
+              placeholder="아이디"
+              error=""
+            />
+            <Input
+              id="아이디"
+              type="text"
+              placeholder="아이디"
+              error="사용자를 찾을 수 없습니다."
+            />
+          </form>
+        </div>
+        
+      </div>
     </div>
   );
 }
---
+++ client/src/components/ui/input.tsx
@@ -0,0 +1,83 @@
+import { forwardRef, useState, type FocusEvent } from "react";
+
+import { cn } from "@/lib/utils"
+import { ErrorMessage } from "@/components/ui/ErrorMessage";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> { 
+    label?: string;
+    error?: string;
+    helperText?: string;
+    variant?: 'default' | 'focused' | 'error';
+  }
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({
+    label,
+    helperText,
+    error,
+    variant = 'default',
+    className,
+    type,
+    onFocus,
+    onBlur,
+    ...props 
+}, ref) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    const currentVariant = error ? 'error' : (isFocused ? 'focused' : variant);
+
+    const handleFocus = (e: FocusEvent<HTMLInputElement>) => {
+      setIsFocused(true);
+      onFocus?.(e);
+    };
+
+    const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+      setIsFocused(false);
+      onBlur?.(e);
+    }
+
+    return (
+      <div>
+        {label && (
+          <label>
+            {label}
+          </label>
+        )}
+        <div className="relative">
+          <input
+            type={type}
+            className={cn(
+              "flex h-[3.125rem] w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              
+              currentVariant === 'default' && "border-gray-300 focus:border-blue-500",
+              currentVariant === 'focused' && "border-blue-500",
+              currentVariant === 'error' && "border-red-500",
+
+              error && "pr-10",
+              props.disabled && "bg-gray-50 text-gray-500 cursor-not-allowed",
+              className,
+
+            )}
+            ref={ref}
+            autoComplete="off"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            {...props}
+          />
+          {error && (
+            <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+              <img src="/image/alert.svg" alt="에러 이미지" />
+            </div>
+          )}
+        </div>
+
+        {error && <ErrorMessage message={error} />}
+        {helperText && !error && <p className="text-sm text-gray-500">{helperText}</p>}
+      </div>
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
