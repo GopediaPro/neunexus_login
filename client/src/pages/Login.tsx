f84c3ae9c75e7ab78f7ce9ec301989cb9ffe90d9
hyeri <yhr4342@naver.com>
2025-06-17 14:00:37 +0900

    feat: 로그인페이지 세팅

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,95 +1,183 @@
+import { useEffect, useState } from 'react';
+import { loginSchema } from '@/schemas/auth.schema';
 import { Input } from '@/components/ui/input';
-import { signupSchema } from '@/schemas/auth.schema';
+import { FormField } from '@/components/ui/FormField';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { FormField } from '@/components/ui/FormField';
-import { Button } from '@/components/ui/Button';
 import { useNavigate } from 'react-router-dom';
-import { keycloakSignup } from '@/services/keycloakSignup';
+import { Button } from '@/components/ui/Button';
+import { keycloakLogin } from '@/services/keycloakLogin';
 import { Checkbox } from '@/components/ui/Checkbox';
+import { useTheme } from 'next-themes';
 
-export type SignupFormData = z.infer<typeof signupSchema>;
+export type LoginFormData = z.infer<typeof loginSchema>;
 
 const Login = () => {
   const navigate = useNavigate();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const getSystemTheme = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  };
 
   const {
     control,
     handleSubmit,
     formState: { errors, isSubmitting },
     setError
-  } = useForm<SignupFormData>({
-    resolver: zodResolver(signupSchema),
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: '',
       password: '',
-      username: 'aasd'
+      rememberMe: 0
     }
   });
 
-  const onSubmit = async (data: SignupFormData) => {
+  const onSubmit = async (data: LoginFormData) => {
     try {
-    } catch (error: any) {}
+      await keycloakLogin(data);
+      console.log('data', data);
+
+      navigate('/');
+    } catch (error: any) {
+      setError('root', {
+        type: 'manual',
+        message: error.message || '로그인 실패'
+      });
+    }
   };
-
+  useEffect(() => {
+    const theme2 = getSystemTheme();
+    setTheme(theme2);
+    console.log('theme', theme);
+  });
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="w-[36.875rem] rounded-lg shadow-lg p-8 border-2">
-        <div className="space-y-4">
-          <img
-            src="/image/logo.svg"
-            alt="로고"
-            className="w-[15rem] h-[8rem]"
-          />
-
-          <div className="flex">
-            <h1 className="text-2xl font-semibold">로그인</h1>
+    <div className="w-full h-screen dark:bg-gray-800 bg-slate-50 flex justify-center items-center">
+      <div className="w-[590px] p-24 dark:bg-gray-700 bg-white rounded-3xl shadow-lg p-24">
+        <div className="flex flex-col items-center gap-10">
+          {/* 로고 */}
+          <div className="w-64 h-16">
+            {theme == 'dark' ? (
+              <img
+                src="/image/logo-dark.svg"
+                alt="로고"
+                className="w-[15rem] h-[8rem]"
+              />
+            ) : (
+              <img
+                src="/image/logo.svg"
+                alt="로고"
+                className="w-[15rem] h-[8rem]"
+              />
+            )}
           </div>
-
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-6"
-          >
+        </div>
+        {/* 로그인 폼 */}
+        <div className="w-full space-y-4">
+          <h1 className="dark:text-white text-neutral-900 text-2xl font-bold">
+            로그인
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
             <FormField
-              name="id"
+              name="email"
               control={control}
-              // 여기 focus 사용위해서 label과 아래 input id 일치 시켜주시면 됨당
-              label="아이디"
-              render={(field, fieldId) => (
+              error={errors.email?.message}
+              render={(field) => (
                 <Input
-                  id={fieldId}
+                  id="이메일"
                   type="email"
-                  placeholder="아이디"
+                  placeholder="email"
+                  error={errors.password?.message}
+                  className="dark:bg-neutral-700 border-neutral-400 bg-white dark:border-zinc-600 dark:text-zinc-200 text-neutral-600 placeholder:text-zinc-400"
                   {...field}
                 />
               )}
-              error={errors.email?.message}
             />
             <FormField
               name="password"
               control={control}
-              label="비밀번호"
-              render={(field, fieldId) => (
+              render={(field) => (
                 <Input
-                  id={fieldId}
+                  id="비밀번호"
                   type="password"
-                  placeholder="비밀번호"
+                  placeholder="password"
+                  error={errors.password?.message}
+                  className="dark:bg-neutral-700 border-neutral-400 bg-white dark:border-zinc-600 dark:text-zinc-200 text-neutral-600 placeholder:text-zinc-400"
                   {...field}
                 />
               )}
               error={errors.password?.message}
             />
+            {/* 자동로그인 & 찾기 옵션 */}
+            <div className="flex justify-between items-center">
+              <label>
+                <FormField
+                  name="rememberMe"
+                  control={control}
+                  render={(field) => (
+                    <div className="flex items-center gap-2 py-3 cursor-pointer">
+                      <Checkbox
+                        checked={field.value === 1}
+                        className="w-4 h-4 rounded border border-neutral-900 dark:border-gray-50 bg-transparent accent-blue-500"
+                        onCheckedChange={(checked) => {
+                          field.onChange(checked ? 1 : 0);
+                        }}
+                      />
+                      <span className="dark:text-gray-50 text-neutral-900 text-sm hover:text-blue-600">
+                        자동 로그인
+                      </span>
+                    </div>
+                  )}
+                />
+              </label>
 
-            {errors.root && <div>{errors.root.message}</div>}
-            <Checkbox />
-            <label>자동 로그인</label>
-            <label>아이디 찾기</label>
-            <label>비밀번호 찾기</label>
-            <Button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? '로그인 스피닝' : '로그인'}
+              <div className="flex items-center gap-4">
+                <button
+                  type="button"
+                  className="py-3 px-2.5 dark:text-gray-50 text-neutral-900 text-sm hover:text-blue-600 transition-colors"
+                >
+                  아이디 찾기
+                </button>
+                <div className="w-px h-3.5 bg-zinc-600"></div>
+                <button
+                  type="button"
+                  className="py-3 px-2.5 dark:text-gray-50 text-neutral-900 text-sm hover:text-blue-600 transition-colors"
+                >
+                  비밀번호 찾기
+                </button>
+              </div>
+            </div>
+
+            <Button
+              type="submit"
+              variant="default"
+              className="btn-login bg-blue-500 transition-colors"
+              disabled={isSubmitting}
+            >
+              <span>로그인</span>
             </Button>
           </form>
+          {/* 회원가입 링크 */}
+          <div className="flex justify-center items-center gap-1 pt-4">
+            <span className="text-neutral-800 dark:text-neutral-50 font-medium">
+              아직 회원이 아니신가요?
+            </span>
+            <button
+              type="button"
+              className="px-2.5 py-3 text-blue-600 dark:text-blue-500 font-bold underline hover:text-blue-400 transition-colors"
+            >
+              회원가입
+            </button>
+          </div>
         </div>
       </div>
     </div>
