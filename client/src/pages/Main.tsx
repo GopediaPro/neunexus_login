4ef8c277252d6e2e8f9f53f5ed2af77342535776
aquaman122 <swj981128@gmail.com>
2025-06-24 17:37:06 +0900

    feat: 메인페이지 다크모드 스타일 추가

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -2,11 +2,13 @@ import { ContentLayout } from "@/components/main/Layout/ContentLayout";
 import { HeaderLayout } from "@/components/main/Layout/HeaderLayout";
 import { LeftSidebarLayout } from "@/components/main/Layout/LeftSidebarLayout";
 import { RightSidebarLayout } from "@/components/main/Layout/RightSidebarLayout";
+import { useTheme } from "next-themes";
 
 const Main = () => {
+  const { theme } = useTheme();
 
   return (
-    <div className="page-main marker:min-h-screen bg-page-bg">
+    <div className={`page-main ${theme === 'dark' ? 'dark' : ""} marker:min-h-screen bg-page-bg`}>
       <div className={`grid grid-sidebar-layout min-h-screen`}>
         <LeftSidebarLayout />
         <div className="flex flex-col">
