6f2dc93127fb54e31f3b0c047eb8ed11a8e76fd5
hyeri <yhr4342@naver.com>
2025-06-25 06:06:43 +0900

    refactor/table style 추가

--- client/src/components/main/ui/TestDashbord.tsx
+++ client/src/components/main/ui/TestDashbord.tsx
@@ -30,12 +30,92 @@ export const TestDashboard = () => {
                 </button>
               ))}
             </div>
+            <div className="tbl-wrap">
+              <div className="tbl-head">
+                <table className="tbl">
+                  <colgroup>
+                    <col width="30%" />
+                    <col width="30%" />
+                    <col width="" />
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th className="text-left">품목코드</th>
+                      <th className="text-right">재고수량</th>
+                      <th className="text-right">재고금액</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+              <div className="tbl-body">
+                <table className="tbl">
+                  <colgroup>
+                    <col width="30%" />
+                    <col width="30%" />
+                    <col width="" />
+                  </colgroup>
+                  <tbody>
+                    <tr>
+                      <td className="text-left">카카오</td>
+                      <td className="text-right">576</td>
+                      <td className="text-right">2,663,936</td>
+                    </tr>
+                    <tr>
+                      <td className="text-left">카카오</td>
+                      <td className="text-right">576</td>
+                      <td className="text-right">2,663,936</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
         <div className="card-right-wrap">
           <div className="card-title">
             <h3>재고현황</h3>
             <ViewAllButton />
+          </div>
+          <div className="card-body">
+            <div className="tbl-wrap">
+              <div className="tbl-head">
+                <table className="tbl">
+                  <colgroup>
+                    <col width="30%" />
+                    <col width="30%" />
+                    <col width="" />
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th className="text-left">품목코드</th>
+                      <th className="text-right">재고수량</th>
+                      <th className="text-right">재고금액</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+              <div className="tbl-body">
+                <table className="tbl">
+                  <colgroup>
+                    <col width="30%" />
+                    <col width="30%" />
+                    <col width="" />
+                  </colgroup>
+                  <tbody>
+                    <tr>
+                      <td className="text-left text-page-error">OCR-SNM1</td>
+                      <td className="text-right">176</td>
+                      <td className="text-right">2,663,936</td>
+                    </tr>
+                    <tr>
+                      <td className="text-left text-page-error">OCT-4T19</td>
+                      <td className="text-right">176</td>
+                      <td className="text-right">2,663,936</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
