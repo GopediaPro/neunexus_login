c59128f210276e5b42d85fa54380405e1f3b79b4
aquaman122 <swj981128@gmail.com>
2025-06-28 16:34:38 +0900

    feat: 초기 로딩시 auth 상태값 초기화 되는 오류 수정

--- client/src/components/layout/PublicRoute.tsx
+++ client/src/components/layout/PublicRoute.tsx
@@ -4,7 +4,15 @@ import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 
 export const PublicRoute = ({ children }: { children: ReactNode }) => {
-  const { isAuthenticated } = useAuthContext();
+  const { isAuthenticated, isLoading } = useAuthContext();
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen gap-4">
+        <div className="w-10 h-10 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+      </div>
+    )
+  }
 
   if (isAuthenticated) {
     return <Navigate to={ROUTERS.MAIN} replace />;
