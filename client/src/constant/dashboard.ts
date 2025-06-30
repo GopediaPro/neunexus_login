73c87c139f15652213fb80fcf22b03703345e76f
aquaman122 <swj981128@gmail.com>
2025-06-30 21:19:22 +0900

    refactoring: StatsDashboard 스타일 수정

---
+++ client/src/constant/dashboard.ts
@@ -0,0 +1,6 @@
+export const statsItems = [
+  { iconName: 'cart', label: '신규주문' },
+  { iconName: 'document', label: '주문확인' },
+  { iconName: 'x', label: '송장 미전송' },
+  { iconName: 'check', label: '송장 전송' }
+];
