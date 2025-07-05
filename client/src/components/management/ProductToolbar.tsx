fc79dcf0d0ceb0d5354bef28f2dfcf7e833217eb
aquaman122 <swj981128@gmail.com>
2025-07-05 12:50:00 +0900

    refactoring: searchbar style edit

--- client/src/components/management/ProductToolbar.tsx
+++ client/src/components/management/ProductToolbar.tsx
@@ -12,15 +12,18 @@ export const ProductToolbar = () => {
 
   return (
     <div className="flex items-center gap-4 px-6 pt-5 bg-page-card-bg">
-      <Input
-        ref={inputRef}
-        type="text"
-        placeholder="전체 검색 (상품명, ID, 고객명 등)"
-        className="w-[320px] pl-12 h-10 bg-gray-200 border-none relative"
-      /> 
-      <Icon name="search" ariaLabel="검색" 
-        onClick={handleIconClick}
-        style="absolute left-9 w-6 h-6 text-page-font-tertiary cursor-pointer" />
+      <div className="flex items-center w-[320px] h-10 bg-gray-200 rounded-md px-3">
+        <Icon name="search" ariaLabel="검색" 
+          onClick={handleIconClick}
+          style="w-5 h-5 text-page-font-tertiary cursor-pointer flex-shrink-0"/>
+        <Input
+          ref={inputRef}
+          type="text"
+          placeholder="전체 검색 (상품명, ID, 고객명 등)"
+          className="w-[280px] pl-4 h-10 bg-gray-200 border-none relative"
+        />
+      </div> 
+
       <div className="flex items-center gap-2">
         <Button variant="light" className="py-5">상품 등록</Button>
         <Button variant="light" className="py-5">판매가 수정</Button>
