68cba821c5a66b42bed38ef84b04a5bcab57d3d5
aquaman122 <swj981128@gmail.com>
2025-07-09 22:43:16 +0900

    fix: height 값 수정

--- client/src/components/mainpage/layout/InfoSidebarLayout.tsx
+++ client/src/components/mainpage/layout/InfoSidebarLayout.tsx
@@ -4,7 +4,7 @@ import { OrganizationContainer } from "../sidebar/OrganizationContainer";
 
 export const InfoSidebarLayout = () => {
   return (
-    <div className={`sidebar-right-width min-w-[293px] min-h-[930px] bg-fill-base-100 p-1 shadow-xl border border-stroke-base-100 rounded-[10px] space-y-6`}>
+    <div className={`sidebar-right-width min-w-[288px] h-[926px] bg-fill-base-100 p-1 shadow-xl border border-stroke-base-100 rounded-[10px] space-y-6`}>
       <NotificationContainer />
       <ScheduleContainer />
       <OrganizationContainer />
