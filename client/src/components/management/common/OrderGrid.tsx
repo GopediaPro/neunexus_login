0f5b1142bb3155ff3d6ce1d2035f76feabdd47cf
aquaman122 <swj981128@gmail.com>
2025-07-09 10:49:15 +0900

    feat: 주문 목록 UI 생성

---
+++ client/src/components/management/common/OrderGrid.tsx
@@ -0,0 +1,16 @@
+
+export const OrderGrid = () => {
+
+  return (
+    <div className="ag-theme-alpine w-full h-[calc(100vh-60px)]">
+      {/* <AgGridReact
+        ref={gridRef}
+        rowData={productData || []}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+        onGridReady={onGridReady}
+        {...gridOptions}
+      /> */}
+    </div>
+  );
+};
