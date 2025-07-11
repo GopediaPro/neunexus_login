9fe6b50e7d1a16719cb5c5d2247a1f56bc6c4b3c
Moon-Yeehwan <milklover516@gmail.com>
2025-07-11 12:19:11 +0900

    fix: ProductLayout에서 RuleEngineToolbar 제거

--- client/src/components/management/layout/ProductLayout.tsx
+++ client/src/components/management/layout/ProductLayout.tsx
@@ -1,5 +1,3 @@
-import { RuleEngineToolbar } from './RuleEngineToolbar';
-
 export const ProductLayout = () => {
   const { isOpen, close } = useSidebar();
   const isInitialMount = useRef(true);
@@ -18,7 +16,6 @@ export const ProductLayout = () => {
           <MenuSidebarLayout />
           <div className="flex flex-col">
             <HeaderManagement title="상품/주문 관리 시스템" />
-            <RuleEngineToolbar />
             <ProductToolbar />
             <div className="flex-1 p-4">
               <ProductGrid />
@@ -28,7 +25,6 @@ export const ProductLayout = () => {
       ) : (
         <div className="flex flex-col min-h-screen bg-fill-base-100">
           <HeaderManagement title="상품/주문 관리 시스템" />
-          <RuleEngineToolbar />
           <ProductToolbar />
           <div className="flex-1 p-4 pl-6">
             <ProductGrid />
