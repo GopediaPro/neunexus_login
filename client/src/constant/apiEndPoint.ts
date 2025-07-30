8fe9fd5b86affc96feafdcf0624777d446dbae67
aquaman122 <swj981128@gmail.com>
2025-07-30 13:59:09 +0900

    fix: 상품등록 조회 endpoint 수정

--- client/src/constant/apiEndPoint.ts
+++ client/src/constant/apiEndPoint.ts
@@ -2,7 +2,7 @@ export const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 const API_VERSION = '/api/v1';
 
 export const API_END_POINT = {
-  PRODUCTS: `${API_VERSION}/products`,
+  PRODUCTS: `${API_VERSION}/product`,
   // 다운폼 주문 기본 엔드포인트
   DOWN_FORM_ORDERS: `${API_VERSION}/down-form-orders`,
   DOWN_FORM_ORDERS_PAGINATION: `${API_VERSION}/down-form-orders/pagination`,
