9042a770edbd792796a842850a8dde4c39f52a96
aquaman122 <swj981128@gmail.com>
2025-07-30 13:57:02 +0900

    feat: 상품등록엑셀가져오기 api 연동

--- client/src/api/product/postProductRegistrationExcelImport.ts
+++ client/src/api/product/postProductRegistrationExcelImport.ts
@@ -3,15 +3,18 @@ import { httpClient } from "@/shared/axios";
 
 export const postProductRegistrationExcelImport = async (file: File, sheetName: string) => {
   const formData = new FormData();
-  formData.append('sheet_name', sheetName);
   formData.append('file', file);
 
-  const response = await httpClient.post(API_END_POINT.PRODUCT_REGISTRATION_EXCEL_IMPORT, formData,
+  const params = new URLSearchParams({
+    sheet_name: sheetName
+  });
+
+  const response = await httpClient.post(`${API_END_POINT.PRODUCT_REGISTRATION_EXCEL_IMPORT}?${params.toString()}`, formData,
     {
       headers: {
         'Content-Type': undefined
       },
-      timeout: 30000
+      timeout: 80000
     }
   );
   return response.data;
