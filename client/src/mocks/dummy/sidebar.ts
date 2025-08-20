7e9f1a35faa26cdac58e0cedf67b36c9325cbcf2
sengwoong <92924243+sengwoong@users.noreply.github.com>
2025-08-20 18:54:14 +0900

    feat: 모킹 연결

--- client/src/mocks/dummy/sidebar.ts
+++ client/src/mocks/dummy/sidebar.ts
@@ -1,23 +1,24 @@
 export const member = [
-  { id: "1", name: "김OO 주임", department: "마케팅 부서" },
-  { id: "2", name: "김OO 주임", department: "마케팅 부서" },
-  { id: "3", name: "김OO 주임", department: "마케팅 부서" },
-  { id: "4", name: "김OO 주임", department: "마케팅 부서" },
-  { id: "5", name: "김OO 주임", department: "마케팅 부서" },
-  { id: "6", name: "김OO 주임", department: "마케팅 부서" },
-  { id: "7", name: "김OO 주임", department: "마케팅 부서" },
-  { id: "8", name: "문OO 주임", department: "마케팅 부서" },
-]
-
-export const notices = [
-  { id: "1", title: "신규 업무 시스템 도입 안내", date: "2025.06.09" },
-  { id: "2", title: "신규 업무 시스템 도입 안내", date: "2025.06.09" },
-  { id: "3", title: "신규 업무 시스템 도입 안내", date: "2025.06.09" },
-  { id: "4", title: "신규 업무 시스템 도입 안내 오버플로우 테스트 진행 고고", date: "2025.06.09" }
-];
-
-export const schedules = [
-  { id: "1", title: "신규 업무 시스템 도입 안내", time: "13:00" },
-  { id: "2", title: "신규 업무 시스템 도입 안내", time: "13:00" },
-  { id: "3", title: "신규 업무 시스템 도입 안내", time: "13:00" }
-];
+    { id: "1", name: "김OO 주임", department: "마케팅 부서" },
+    { id: "2", name: "김OO 주임", department: "마케팅 부서" },
+    { id: "3", name: "김OO 주임", department: "마케팅 부서" },
+    { id: "4", name: "김OO 주임", department: "마케팅 부서" },
+    { id: "5", name: "김OO 주임", department: "마케팅 부서" },
+    { id: "6", name: "김OO 주임", department: "마케팅 부서" },
+    { id: "7", name: "김OO 주임", department: "마케팅 부서" },
+    { id: "8", name: "문OO 주임", department: "마케팅 부서" },
+  ]
+  
+  export const notices = [
+    { id: "1", title: "신규 업무 시스템 도입 안내", date: "2025.06.09" },
+    { id: "2", title: "신규 업무 시스템 도입 안내", date: "2025.06.09" },
+    { id: "3", title: "신규 업무 시스템 도입 안내", date: "2025.06.09" },
+    { id: "4", title: "신규 업무 시스템 도입 안내 오버플로우 테스트 진행 고고", date: "2025.06.09" }
+  ];
+  
+  export const schedules = [
+    { id: "1", title: "신규 업무 시스템 도입 안내", time: "13:00" },
+    { id: "2", title: "신규 업무 시스템 도입 안내", time: "13:00" },
+    { id: "3", title: "신규 업무 시스템 도입 안내", time: "13:00" }
+  ];
+  
